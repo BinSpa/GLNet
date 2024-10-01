28a32a854a02120ec894aa4ee85ad736bb58082d
BinSpa <luogu1024116558@gmail.com>
2024-10-01 10:22:15 +0800

    changes

---
+++ eval_gid.sh
@@ -0,0 +1,16 @@
+python train_deep_globe.py \
+--n_class 6 \
+--dataset "gid" \
+--data_path "/data1/gyl/RS_DATASET/FBP" \
+--model_path "../../saved_models/gid" \
+--log_path "../../runs/gid" \
+--task_name "eval" \
+--mode 3 \
+--batch_size 6 \
+--sub_batch_size 32 \
+--size_g 508 \
+--size_p 508 \
+--path_g "gidfpn_gid_global120.pth" \
+--path_g2l "gidfpn_gid_global2local50.pth" \
+--path_l2g "gidfpn_gid_local2global58.pth" \
+--evaluation
